d1b853b0aaa04b90be9bf832291b037f8c09facf
Chris Scavello <bideowego@gmail.com>
2015-07-16 04:27:56 -0400

    Add indenting script for Google code prettyprint

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,3 +1,26 @@
 //=require application
 //=require_tree ./frontend
 //=require_self
+
+(function($) {
+
+	window.exports = [];
+	window.exports["indent"] = function() {
+		var prettyprinted = $('.prettyprinted li span:first-child');
+		for (var i = 0; i < prettyprinted.length; i++) {
+			var line = prettyprinted[i];
+			var text = line.innerHTML;
+			var indentationLevel = 0;
+			for (var j = 0; j < text.length; j++) {
+				var char = text[j];
+				if (char != ' ')
+					break;
+				indentationLevel++;
+			}
+			if (indentationLevel > 0) {
+				$(prettyprinted[i]).addClass('indent-' + indentationLevel);
+			}
+		}
+	};
+
+})(jQuery);
